e94ce19c7daff72fcee7ff77253ad4c29cdcec7e
Bjorn Hansen <holomorph@gmail.com>
2022-07-04 12:48:38 -0700

    keep the currentCode variable in the main source. make RESET_KEY a const auto-format the code

--- GarageKeypadSketch.ino
+++ GarageKeypadSketch.ino
@@ -1,18 +1,20 @@
 /*
   May control my garage door using a keypad
 */
- 
+
 #include <Keypad.h>
 
-// I want to keep the secret code in another file so that I can 
-// make the rest of the source code public.  
+// I want to keep the secret code in another file so that I can
+// make the rest of the source code public.
 // Include the secret code with:
 #include "secretcode.h"
 //which should define the secret like this:
 //
 //const byte CODELENGTH = 5;
 //const String correctCode = "12345";
-//      String currentCode = "00000";
+
+char currentCode[sizeof(correctCode)];
+const char RESET_KEY = '*';
 
 const byte ROWS = 4; //four rows
 const byte COLS = 3; //three columns
@@ -20,10 +22,10 @@ const byte COLS = 3; //three columns
 byte index = 0;
 
 char keys[ROWS][COLS] = {
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'},
-  {'*','0','#'}
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
 };
 byte rowPins[ROWS] = {8, 7, 6, 5}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {4, 3, 2}; //connect to the column pinouts of the keypad
@@ -32,59 +34,58 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 const int ledPin =  12;
 
-void setup(){
+void setup() {
   Serial.begin(9600);
   keypad.addEventListener(keypadEvent);
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
 }
 
-void loop(){
+void loop() {
   keypad.getKey();
 }
- 
-void keypadEvent(KeypadEvent key){
-    if (keypad.getState() == PRESSED){
-        Serial.print(key);
-        Serial.print(' ');
-        Serial.println("PRESSED");
-        RecordKey(key);
-    }
+
+void keypadEvent(KeypadEvent key) {
+  if (keypad.getState() == PRESSED) {
+    Serial.print(key);
+    Serial.print(' ');
+    Serial.println("PRESSED");
+    RecordKey(key);
+  }
 }
 
-void RecordKey(char key){
-  if('*' == key){
+void RecordKey(char key) {
+  if (RESET_KEY == key) {
     index = 0;
     return;
   }
-  
+
   currentCode[index] = key;
   index++;
-  
-  if(index == CODELENGTH){
+
+  if (index == CODELENGTH) {
     checkCode();
     index = 0;
   }
 }
 
-void checkCode(){
+void checkCode() {
   Serial.print("checking code: ");
   Serial.println(currentCode);
-  
-  if(currentCode == correctCode){
+
+  if (correctCode.equals(currentCode)) {
     Serial.println("code accepted");
     OpenTheDoor();
   }
-  else{
+  else {
     Serial.println("code rejected");
   }
 }
 
-void OpenTheDoor(){
+void OpenTheDoor() {
   Serial.println("closing relay . . .");
   digitalWrite(ledPin, LOW);
   delay(500);
   digitalWrite(ledPin, HIGH);
   Serial.println("opening relay");
 }
-
